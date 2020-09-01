0b6beff083bd69acd31e2930f79cc091e192ff67
Lev Kim <irene.kim@revature.net>
2020-08-31 23:09:04 -0400

    edited sql setup file to ensure all db references work

--- table-setup.sql
+++ table-setup.sql
@@ -1,18 +1,18 @@
-drop table if exists ers_user_roles;
-drop table if exists ers_reimbursement_status;
-drop table if exists ers_reimbursement_type;
+drop table if exists ers_user_roles cascade;
+drop table if exists ers_reimbursement_status cascade;
+drop table if exists ers_reimbursement_type cascade;
 drop table if exists ers_users cascade;
 drop table if exists ers_reimbursement cascade;
 
 
 create table ers_user_roles (
-	ers_user_role_id serial primary key,
+	ers_user_role_id int generated always as identity primary key,
 	user_role varchar(10)
 );
 
 create table ers_users (
-	ers_users_id serial primary key,
-	ers_username varchar(50) unique, 
+	ers_users_id int generated always as identity primary key,
+	ers_username varchar(50) unique,
 	ers_password varchar(50),
 	user_first_name varchar(100),
 	user_last_name varchar(100),
@@ -21,17 +21,17 @@ create table ers_users (
 );
 
 create table ers_reimbursement_status (
-	reimb_status_id serial primary key,
+	reimb_status_id int generated always as identity primary key,
 	reimb_status varchar(10)
 );
 
 create table ers_reimbursement_type (
-	reimb_type_id serial primary key,
+	reimb_type_id int generated always as identity primary key,
 	reimb_type varchar(10)
 );
 
 create table ers_reimbursement (
-	reimb_id serial primary key,
+	reimb_id int generated always as identity primary key,
 	reimb_amount integer,
 	reimb_submitted timestamp,
 	reimb_resolved timestamp,
@@ -42,3 +42,11 @@ create table ers_reimbursement (
 	reimb_status_id integer references ers_reimbursement_status(reimb_status_id),
 	reimb_type_id integer references ers_reimbursement_type(reimb_type_id)
 );
+
+insert into ers_user_roles (user_role) values ('MANAGER');
+insert into ers_user_roles (user_role) values ('EMPLOYEE');
+
+insert into ers_reimbursement_type (reimb_type) values ('TRAVEL');
+insert into ers_reimbursement_type (reimb_type) values ('LODGING');
+insert into ers_reimbursement_type (reimb_type) values ('FOOD');
+insert into ers_reimbursement_type (reimb_type) values ('OTHER');
